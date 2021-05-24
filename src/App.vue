e29022d31b6a2c0292df8c968fed41c62f146b04
Levi Murray <levi@levimurray.dev>
2021-05-24 01:34:59 +0100

    Added WoW bfa icon + basic router nav

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="split-container">
+      <div id="nav">
+        <app-router/>
+      </div>
+      <div class="selected-content">
+        <router-view/>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import AppRouter from '@/components/AppRouter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppRouter
+  }
+}
+</script>
+
 <style lang="scss">
+@import "./scss/variables";
+
+html, body {
+  padding: 0;
+  margin: 0;
+  background: $bg-blue-dark;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
+  color: white;
+  height: 100vh;
+  width: 100%;
+  padding: 64px 96px;
 
-#nav {
-  padding: 30px;
+  .split-container {
+    display: flex;
+    height: 100%;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    #nav {
+      flex-grow: 1;
+      max-width: 324px;
+    }
 
-    &.router-link-exact-active {
-      color: #42b983;
+    .selected-content {
+      flex-grow: 6;
     }
   }
+
 }
+
 </style>
---
+++ src/components/AppRouter.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app-router">
+    <div class="bfa">
+      <img src="../assets/wow-bfa.png" alt="World of Warcraft - Battle for Azeroth">
+    </div>
+    <div class="app-links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/horde">Horde</router-link>
+      <router-link to="/alliance">Alliance</router-link>
+      <router-link to="/media">Media</router-link>
+      <router-link to="/trailer">Trailer</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppRouter',
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../scss/variables";
+
+  .app-router {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+
+    .bfa {
+      img {
+        width: 100%;
+      }
+    }
+
+    .app-links {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      flex-grow: 1;
+      padding: 128px 0;
+
+      a {
+        color: $text-blue;
+        opacity: 0.6;
+        text-decoration: none;
+        text-transform: uppercase;
+
+        &.router-link-exact-active {
+          opacity: 1;
+          font-weight: bold;
+          text-shadow: 0 0 34px currentColor;
+        }
+      }
+    }
+
+  }
+</style>
